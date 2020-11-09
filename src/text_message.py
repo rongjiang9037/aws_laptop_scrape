4d1ef5d1a0b4685b9a2c34f19c0a283e34ac7549
rongjiang9037 <rong_jiang@126.com>
2020-11-09 16:50:52 +0000

    delete Twilio account credentials from git

--- src/text_message.py
+++ src/text_message.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
   
 # Your Account SID from twilio.com/console
-account_sid = "ACde61f88f78c4d03db0afa78ba187082a"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "5d21421134ababd20b3676f0ca2b2da2"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
